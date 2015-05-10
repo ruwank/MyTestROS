bc9211467ce836789aebc98048e040477ed6bf97
Suresh <sureshsumanasooriya@gmail.com>
2015-05-10 22:45:02 +0530

    App enter flow

--- app/src/main/java/com/relianceit/relianceorder/util/ConnectionDetector.java
+++ app/src/main/java/com/relianceit/relianceorder/util/ConnectionDetector.java
@@ -6,46 +6,13 @@ import android.net.NetworkInfo;
 
 public class ConnectionDetector {
 
-	private Context _context;
+    public static NetworkInfo getNetworkInfo(Context context){
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
 
-	public ConnectionDetector(Context context) {
-		this._context = context;
-	}
-
-	/**
-	 * Checking for all possible internet providers
-	 * **/
-	public boolean isConnectingToInternet() {
-		ConnectivityManager connectivity = (ConnectivityManager) _context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (connectivity != null) {
-			NetworkInfo[] info = connectivity.getAllNetworkInfo();
-			if (info != null)
-				for (int i = 0; i < info.length; i++)
-					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-						return true;
-					}
-
-		}
-		return false;
-	}
-
-	public NetworkType getNetworkType() {
-		NetworkType type = NetworkType.TYPE_NONE;
-		ConnectivityManager manager = (ConnectivityManager) _context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo info = manager.getActiveNetworkInfo();
-		if (info != null && info.isAvailable()) {
-			int nType = info.getType();
-			if (nType == ConnectivityManager.TYPE_MOBILE)
-				type = NetworkType.TYPE_MOBILE;
-			else if (nType == ConnectivityManager.TYPE_WIFI)
-				type = NetworkType.TYPE_WI_FI;
-		}
-		return type;
-	}
-
-	public enum NetworkType {
-		TYPE_NONE, TYPE_MOBILE, TYPE_WI_FI
-	}
+    public static boolean isConnected(Context context){
+        NetworkInfo info = ConnectionDetector.getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
 }
